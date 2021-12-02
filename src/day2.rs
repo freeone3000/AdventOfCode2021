507ff23bd70539c5cbfda13c8acec78d1bc82592
Jasmine Moore <freeone3000@gmail.com>
2021-12-02 08:34:42 -0500

    Day 2. Probably should have rewritten on discovery, but this is good enough for now.

--- src/day2.rs
+++ src/day2.rs
@@ -3,13 +3,13 @@ use std::ops::AddAssign;
 use crate::common;
 
 struct Position {
-    depth: i32,
+    aim: i32,
     pos: i32
 }
 
 impl AddAssign for Position {
     fn add_assign(&mut self, rhs: Self) {
-        self.depth += rhs.depth;
+        self.aim += rhs.aim;
         self.pos += rhs.pos;
     }
 }
@@ -17,21 +17,22 @@ impl AddAssign for Position {
 impl FromStr for Position {
     type Err = String;
 
+    // note that depth is uncal
     fn from_str(line: &str) -> Result<Self, Self::Err> {
         let mut wh_iter = line.split_whitespace();
         let token = wh_iter.next().unwrap();
         let amt = i32::from_str(wh_iter.next().unwrap()).unwrap();
         match token {
             "up" => Ok(Position {
-                depth: -amt,
+                aim: -amt,
                 pos: 0
             }),
             "down" => Ok(Position {
-                depth: amt,
+                aim: amt,
                 pos: 0
             }),
             "forward" => Ok(Position {
-                depth: 0,
+                aim: 0,
                 pos: amt
             }),
             _ => Err("Could not determine token type".to_string())
@@ -41,7 +42,7 @@ impl FromStr for Position {
 
 fn calculate_offsets(offsets: Vec<Position>) -> Position {
     let mut current  = Position {
-        depth: 0,
+        aim: 0,
         pos: 0
     };
     for offset in offsets {
@@ -50,14 +51,40 @@ fn calculate_offsets(offsets: Vec<Position>) -> Position {
     current
 }
 
+fn calculate_using_aim(offsets: Vec<Position>) -> (Position, i32) {
+    //depth is uncalculable as an offset so we have it as a local
+    let mut depth = 0;
+    let mut state = Position {
+        aim: 0,
+        pos: 0
+    };
+
+    for offset in offsets {
+        if offset.pos != 0 { // if we move forward, also increase depth by pos * aim
+            depth += offset.pos * state.aim;
+        }
+        state += offset;
+    }
+    (state, depth)
+}
+
 fn parse_line(line: &str) -> Position {
     Position::from_str(line).unwrap()
 }
 
 pub fn part1() -> i32{
     let sample_result = calculate_offsets(common::read_file("day2_sample.txt", &parse_line));
-    assert_eq!(sample_result.depth * sample_result.pos, 150);
+    assert_eq!(sample_result.aim * sample_result.pos, 150);
 
     let result = calculate_offsets(common::read_file("day2.txt", &parse_line));
-    return result.depth * result.pos;
+    return result.aim * result.pos;
+}
+
+pub fn part2() -> i32 {
+    let (d_state, d_depth) = calculate_using_aim(common::read_file("day2_sample.txt", &parse_line));
+    assert_eq!(d_state.pos, 15);
+    assert_eq!(d_depth, 60);
+
+    let (state, depth) = calculate_using_aim(common::read_file("day2.txt", &parse_line));
+    state.pos * depth
 }
--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,6 @@ fn main() {
         println!("Day 1 part 2: {}", day1::part2());
     } else if day == 2 {
         println!("Day 2 part 1: {}", day2::part1());
+        println!("Day 2 part 1: {}", day2::part2());
     }
 }
