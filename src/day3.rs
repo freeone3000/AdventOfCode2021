51c447eb1a7f41f498881c87dbf86fe0abb66314
Jasmine Moore <freeone3000@gmail.com>
2021-12-03 09:45:39 -0500

    Day 3 part 2. Doesn't quite work? Look into the retain function.

--- src/day3.rs
+++ src/day3.rs
@@ -10,7 +10,7 @@ fn binary_chars_to_number(bchar : &Vec<char>) -> i32 {
     i32::from_str_radix(&mystr, 2).unwrap()
 }
 
-fn parse_numbers(lines: Vec<Vec<char>>) -> i32 {
+fn part1_parse_numbers(lines: Vec<Vec<char>>) -> i32 {
     let mut gamma : Vec<char> = Vec::new();
     let mut epsilon : Vec<char> = Vec::new();
 
@@ -32,9 +32,52 @@ fn parse_numbers(lines: Vec<Vec<char>>) -> i32 {
     binary_chars_to_number(&gamma) * binary_chars_to_number(&epsilon)
 }
 
+fn part2_parse_numbers(lines: Vec<Vec<char>>) -> i32 {
+    let oxygen = retain_from_list(&lines, |freq_table: [i32; 2]| if freq_table[0] > freq_table[1] { '0' } else { '1' });
+    let co2 = retain_from_list(&lines, |freq_table: [i32; 2]| if freq_table[1] < freq_table[0] { '1' } else { '0' });
+    return oxygen * co2;
+}
+
+fn retain_from_list(lines: &Vec<Vec<char>>, f: fn ([i32; 2]) -> char) -> i32 {
+    let mut candidates = lines.clone();
+    let max = candidates[0].len();
+
+    let mut freq_table = [0, 0];
+
+    for i in 0..max {
+        for line in lines {
+            let idx = match line[0] {
+                '0' => 0,
+                '1' => 1,
+                _ => { panic!("Weird input!") }
+            };
+
+            freq_table[idx] += 1;
+        }
+
+        let keep = f(freq_table);
+        candidates.retain(|x| x[i] == keep);
+        if candidates.len() == 1 {
+            return binary_chars_to_number(&candidates[0]);
+        }
+
+        freq_table[0] = 0;
+        freq_table[1] = 0;
+    }
+
+    panic!("Have multiple candidates remaining!!")
+}
+
 pub fn part1() -> i32 {
-    let run = |s| parse_numbers(common::read_file(s, &num_to_digits));
+    let run = |s| part1_parse_numbers(common::read_file(s, &num_to_digits));
     assert_eq!(run("day3_sample.txt"), 198);
+
+    run("day3.txt")
+}
+
+pub fn part2() -> i32 {
+    let run = |s| part2_parse_numbers(common::read_file(s, &num_to_digits));
+    assert_eq!(run("day3_sample.txt"), 230);
 
     run("day3.txt")
 }
--- src/main.rs
+++ src/main.rs
@@ -17,5 +17,6 @@ fn main() {
         println!("Day 2 part 1: {}", day2::part2());
     } else if day == 3 {
         println!("Day 3 part 1: {}", day3::part1());
+        println!("Day 3 part 2: {}", day3::part2());
     }
 }
