d740afe3d9ab3c665c730018939d7bf6624fa188
Jasmine Moore <freeone3000@gmail.com>
2021-12-25 05:40:14 -0500

    Day 25 -- implemented strong currents taking advantage of useful properties of integers

--- src/day25.rs
+++ src/day25.rs
@@ -27,10 +27,13 @@ fn next_step(prev_step: &Array2D<char>) -> Array2D<char> {
     for row in 0..max_row {
         for col in 0..max_col {
             if prev_step[(row, col)] == EAST_FACING {
-                if col + 1 < max_col && prev_step[(row, col+1)] == EMPTY { // move it!
-                    east_result[(row, col)] = EMPTY;
-                    east_result[(row, col+1)] = EAST_FACING;
-                } else { // can't move it
+                if col + 1 < max_col && prev_step[(row, col+1)] == EMPTY { // can be moved
+                    // east_result[(row, col)] = EMPTY; // NOTE: Empty is the default value
+                    east_result[(row, col + 1)] = EAST_FACING;
+                } else if col + 1 == max_col && prev_step[(row, 0)] == EMPTY { // can be moved ("strong currents")
+                    // east_result[(row, col)] = EMPTY;
+                    east_result[(row, 0)] = EAST_FACING;
+                } else { // cannot be moved
                     east_result[(row, col)] = EAST_FACING;
                 }
             } else if prev_step[(row, col)] == SOUTH_FACING { // copy over south
@@ -47,8 +50,11 @@ fn next_step(prev_step: &Array2D<char>) -> Array2D<char> {
                 south_result[(row, col)] = EAST_FACING
             } else if east_result[(row, col)] == SOUTH_FACING {
                 if row + 1 < max_row && east_result[(row+1, col)] == EMPTY { // can be moved
-                    south_result[(row, col)] = EMPTY;
-                    south_result[(row+1, col)] = SOUTH_FACING;
+                    // south_result[(row, col)] = EMPTY; // NOTE: Empty is the default value
+                    south_result[(row + 1, col)] = SOUTH_FACING;
+                } else if row + 1 == max_row && east_result[(0, col)] == EMPTY { // can be moved ("strong currents")
+                    // south_result[(row, col)] = EMPTY;
+                    south_result[(0, col)] = SOUTH_FACING;
                 } else { // can't be moved
                     south_result[(row, col)] = SOUTH_FACING;
                 }
@@ -70,9 +76,10 @@ fn print_step(step: &Array2D<char>) {
 }
 
 pub fn part1() -> i32 {
-    let mut cur_step = parse_input(Path::new("day25_east_south_conflict.txt"));
+    let mut cur_step = parse_input(Path::new("day25_sample.txt"));
     let mut stepno = 0;
     loop {
+        stepno += 1;
         print_step(&cur_step);
         println!();
 
@@ -81,7 +88,6 @@ pub fn part1() -> i32 {
             break;
         }
         cur_step = next_step;
-        stepno += 1;
     }
     stepno
 }
